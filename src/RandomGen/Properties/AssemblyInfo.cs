c54ba6674f827628c7678963a2673945a30f6cf4
Ali Kheyrollahi <aliostad@gmail.com>
2015-06-21 12:48:07 +0100

    version 0.3.3

--- src/RandomGen/Properties/AssemblyInfo.cs
+++ src/RandomGen/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ali Kheyrollahi, Jakub Konecki")]
 [assembly: AssemblyProduct("RandomGen")]
-[assembly: AssemblyCopyright("Copyright © 2014 - Ali Kheyrollahi, Jakub Konecki")]
+[assembly: AssemblyCopyright("Copyright © 2015 - Ali Kheyrollahi, Jakub Konecki")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
-[assembly: AssemblyInformationalVersion("0.3.2")]
+[assembly: AssemblyVersion("0.3.3.0")]
+[assembly: AssemblyFileVersion("0.3.3.0")]
+[assembly: AssemblyInformationalVersion("0.3.3")]
