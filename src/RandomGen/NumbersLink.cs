07a4178fa36a7f46b2909ca1d68a5483b7dc7525
Jakub Konecki <jkonecki@gmail.com>
2021-09-24 08:07:27 +0100

    Update NumbersLink.cs

--- src/RandomGen/NumbersLink.cs
+++ src/RandomGen/NumbersLink.cs
@@ -35,6 +35,32 @@ namespace RandomGen
             return () => random.Next(min, max);
         }
 
+        public Func<IEnumerable<int>> IntegersDrawNoPlacement(int min = 0, int max = 100, int take = 1)
+        {
+            if (min >= max)
+                throw new ArgumentOutOfRangeException("min >= max");
+            if (take > (max - min))
+                throw new ArgumentOutOfRangeException("available < take");
+
+            var pool = Enumerable.Range(0, max - min).ToArray();
+            var result = new List<int>();
+            var random = this._genLink.CreateRandom();
+            
+            //Modern Fisher–Yates shuffle with O(n) complexity
+            for (int i = 0; i < take; i++)
+            {
+                var itemIndex = random.Next(min, max - i);
+                
+                // swap taken index and current last
+                var buffer = pool[itemIndex];
+                pool[itemIndex] = pool[max - i - 1];
+                pool[max - i - 1] = buffer;
+                result.Add(buffer + min);
+            }
+
+            return () => result;
+        }
+        
         public Func<uint> UnsignedIntegers(uint min = 0, uint max = 100)
         {
             if (min >= max)
@@ -85,30 +111,6 @@ namespace RandomGen
             var random = this._genLink.CreateRandom();
 
             return () => min + (range * (decimal)random.NextDouble());
-        }
-
-        public Func<IEnumerable<int>> IntegersDrawNoPlacement(int min = 0, int max = 100, int take_number = 1)
-        {
-            if (min >= max)
-                throw new ArgumentOutOfRangeException("min >= max");
-            if (take_number > (max - min))
-                throw new ArgumentOutOfRangeException("available < take_number");
-
-            var pool = Enumerable.Range(0, max - min).ToArray();
-            var result = new List<int>();
-            var random = this._genLink.CreateRandom();
-            //Modern Fisher–Yates shuffle with O(n) complexity
-            for (int i = 0; i < take_number; i++)
-            {
-                var take = random.Next(min, max - i);
-                // swap taken index and current last
-                var buffer = pool[take];
-                pool[take] = pool[max - i - 1];
-                pool[max - i - 1] = buffer;
-                result.Add(buffer + min);
-            }
-
-            return () => result;
         }
     }
 }
