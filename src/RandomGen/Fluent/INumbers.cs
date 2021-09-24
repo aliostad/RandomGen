c3bee8d15f3cde2ca9cf17d895b484105b2a2578
Jakub Konecki <jkonecki@gmail.com>
2021-09-24 08:04:15 +0100

    Update INumbers.cs

--- src/RandomGen/Fluent/INumbers.cs
+++ src/RandomGen/Fluent/INumbers.cs
@@ -7,12 +7,12 @@ namespace RandomGen.Fluent
     {
         Func<byte> Bytes(byte min = byte.MinValue, byte max = byte.MaxValue);
         Func<int> Integers(int min = 0, int max = 100);
+        Func<IEnumerable<int>> IntegersDrawNoPlacement(int min = 0, int max = 100, int take = 1);
         Func<uint> UnsignedIntegers(uint min = 0, uint max = 100);
         Func<long> Longs(long min = 0, long max = 100);
         IDouble Doubles();
         Func<double> Doubles(double min, double max);
         Func<decimal> Decimals(decimal min = 0, decimal max = 1);
         Func<bool> Booleans();
-        Func<IEnumerable<int>> IntegersDrawNoPlacement(int min = 0, int max = 100, int take_number = 1);
     }
 }
